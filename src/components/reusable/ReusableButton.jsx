f160df77fc0d3c9daec8710f643ca7a00632af1b
najimhaq <mdnajimulhaque@gmail.com>
2026-07-15 19:56:17 +0900

    some changes reusable button

--- src/components/reusable/ReusableButton.jsx
+++ src/components/reusable/ReusableButton.jsx
@@ -1,10 +1,8 @@
-// ReusableButton.jsx
 const ReusableButton = ({ children, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className='bg-cyan-600 text-white px-4 py-2 rounded-md font-semibold
-                 hover:bg-cyan-700 transition cursor-pointer'
+      className='bg-cyan-600 text-white px-4 py-2 rounded-md font-semibold hover:bg-cyan-700 transition cursor-pointer'
     >
       {children}
     </button>
